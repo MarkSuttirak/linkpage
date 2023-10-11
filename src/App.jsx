f6b444759682bfbce2b976c444c956a7abe5dd37
MarkSuttirak <suttirak.ch@zaviago.com>
2023-10-11 14:09:42 +0700

    Commit Codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
 import './App.scss'
 import { FrappeProvider } from 'frappe-react-sdk'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate, useLocation  } from 'react-router-dom'
 import Home from './pages/home'
 import Register from './pages/register'
 import Profile from './pages/profile'
@@ -10,22 +9,66 @@ import EditProfile from './pages/edit-profile'
 import ViewProfile from './pages/view-profile'
 import Setup from './pages/setup'
 import apifetch from './assets/key'
+import { UserProvider } from './hooks/userUser'
+import { getToken, setToken } from "./utils/helper";
+import Cookies from 'js-cookie';
 
 function App() {
+
+  const navigate = useNavigate();
+  const search = useLocation().search;
+  const token = new URLSearchParams(search).get("token");
+  const phoneverify = new URLSearchParams(search).get("phoneverify");
+  const username = new URLSearchParams(search).get("username");
+  const [Userverify, SetUserverify] = useState(phoneverify);
+  const isPhoneVerified = Cookies.get('phoneverify') === 'true';
+
+
+  /*useEffect(() => {
+
+    if (isPhoneVerified) {
+      navigate('/setup');
+    }
+
+    if (token) {
+      Cookies.set('username', username);
+      if (phoneverify == 'true') {
+        Cookies.set('phoneverify', true);
+        navigate('/register');
+      }
+      else {
+        navigate('/setup');
+      }
+
+      if (Cookies.get('system_user') != 'yes') {
+        setToken(token)
+        navigate('/profile');
+        window.location.reload(true);
+      }
+
+
+    }
+
+
+    if (!getToken()) {
+      navigate("/setup");
+    }
+
+
+  },[isPhoneVerified]);*/
+
   return (
     <FrappeProvider 
     url={import.meta.env.VITE_ERP_URL}
     enableSocket={false}
     tokenParams={{
-    useToken: true,
-        token : () => {
-          return `${apifetch.key}:${apifetch.secret}`
-        },
-        type: "token"
+      type: "token",
+      useToken: true,
+      token: getToken,
 
      }} 
      >
-      <BrowserRouter>
+      
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/register' element={<Register />}/>
@@ -34,7 +77,7 @@ function App() {
           <Route path='/view-profile' element={<ViewProfile />}/>
           <Route path='/setup' element={<Setup></Setup>}></Route>
         </Routes>
-      </BrowserRouter>
+  
     </FrappeProvider>
   )
 }
